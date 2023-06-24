ee728eb45b083e6d8417790ed8cd753d6f4881ff
Johnny Pestana <johnnypestana@MacBook-Air-de-Johnny.local>
2023-06-23 21:02:43 -0300

    testing: component template testing

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -22,4 +23,51 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Button: action 1', () => {
+
+    let btnEl: DebugElement;
+    
+    beforeEach(() => {
+      btnEl = fixture.debugElement.query(By.css('button#act1'));
+    });
+
+    it('should have button', () => {
+      expect(btnEl).not.toBeFalsy();
+    });
+
+    it('should show message when btn is clicked', () => {
+      btnEl.nativeElement.click();
+
+      fixture.detectChanges();
+
+      const messageEl = fixture.debugElement.query(By.css('span'));
+      expect(messageEl.nativeElement.innerText).toEqual('action 1 done');
+    });
+    
+  });
+
+  describe('Button: action 2', () => {
+
+    let btnEl: DebugElement;
+
+    beforeEach(() => {
+      btnEl = fixture.debugElement.query(By.css('button#act2'));
+    });
+
+    it('should have action 2 button', () => {
+      expect(btnEl).not.toBeFalsy();
+    });
+
+    it('should show message when btn is clicked', () => {
+      btnEl.nativeElement.click();
+
+      fixture.detectChanges();
+
+      const message = fixture.debugElement.query(By.css('span'));
+      expect(message.nativeElement.innerText).toEqual('action 2 done');
+    });
+
+  });
+  
 });
