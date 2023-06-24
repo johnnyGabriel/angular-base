a4ac62728cb94951233a0725ea2b50e5468975a1
Johnny Pestana <johnnygpestana@gmail.com>
2023-06-23 21:28:22 -0300

    routing: router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }
 ];
 
